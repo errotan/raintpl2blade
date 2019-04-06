7593d8bf36be2e0ab069a94bc4c57569c4f0adfa
Puskás Zsolt <errotan@gmail.com>
2019-04-06 20:22:41 +0200

    finish dir convert script

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ RainTPL to Blade converter
 
 Converts RainTPL template files to Blade files used in Laravel framework.
 
-Uses code of the RainTPL project. Copyright (c) 2011-2017 Federico Ulfo <rainelemental@gmail.com>
+Usage: `php convert.php [<extension>] <directory>`
+
+The `tests` directory contains some sample files to try.
+
+This project uses code from the RainTPL project. Copyright (c) 2011-2017 Federico Ulfo <rainelemental@gmail.com>
 
 Licensed under the [MIT license](LICENSE).
--- convert.php
+++ convert.php
@@ -11,7 +11,33 @@ ini_set('display_errors', true);
 require 'RainTPL_SyntaxException.php';
 require 'RainTPL2Blade.php';
 
-$contents = file_get_contents('tests/test.html');
-$tp = new RainTPL2Blade($contents);
+if (1 === $argc) {
+    echo 'Usage: php convert.php [<extension>] <directory>'."\n";
+    exit(0);
+} else if (2 === $argc) {
+    $extension = 'html';
+    $directory = $argv[1];
+} else if (3 === $argc) {
+    $extension = $argv[1];
+    $directory = $argv[2];
+}
 
-file_put_contents('tests/test.blade.php', $tp->convert());
+$directory = new RecursiveDirectoryIterator($directory);
+$iterator = new RecursiveIteratorIterator($directory);
+$regex = new RegexIterator($iterator, '/.+\.' . $extension . '$/i', RecursiveRegexIterator::GET_MATCH);
+$counter = 0;
+
+foreach ($regex as $filename) {
+    $filename = $filename[0];
+    $tp = new RainTPL2Blade(file_get_contents($filename));
+    $filename = dirname($filename) . '/' . pathinfo($filename, PATHINFO_FILENAME);
+
+    try {
+        file_put_contents($filename . '.blade.php', $tp->convert());
+        $counter++;
+    } catch (Exception $e) {
+        echo 'Exception occurred at file: ' . $filename . ' > ' .  $e->getMessage() . "\n";
+    }
+}
+
+echo 'Converted ' . $counter . ' file(s).'."\n";
